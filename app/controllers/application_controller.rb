34b636fa97e9b1d6752def7abc7801fb2af13d8f
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-29 16:10:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,9 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
-    
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
   end
 end
